d183cf4cb61a76c39d8b1fddf871a1ee26ca18ab
oncee <oncee0210@gmail.com>
2022-12-06 17:20:14 +0900

    혼자 구현한 소스

--- main.js
+++ main.js
@@ -7,3 +7,55 @@
 //5번의 기회를 다쓰면 게임이 끝난다 (더이상 추측 불가, go버튼이 disabled)
 //유저가 1~50 밖에 숫자를 입력하면 알려준다. (기회는 줄지 않는다)
 //유저가 이미 입력한 숫자를 입력하면 알려준다. (기회는 줄지 않는다)
+
+
+//랜덤번호 지정
+const $resultText = document.querySelector('#result-text');
+const $goButton = document.querySelector('#btn-go');
+const $resetButton = document.querySelector('#btn-reset');
+const $inputNum = document.querySelector("input[name=number]");
+let inputNum_val = "";
+let inputNum_log = [];
+
+let randomNum = 0;
+let result = "";
+let chance = 5;
+
+function setRandom() {
+  return Math.floor(Math.random() * 50) + 1;
+}
+randomNum = setRandom();
+
+$goButton.addEventListener('click', function(){
+  inputNum_val = $inputNum.value;
+
+  if(inputNum_val <=0 || inputNum_val > 50){
+    $resultText.innerText = '1에서 50사이 숫자를 입력해주세요. (기회: '+chance+')';
+  }else{
+    if(inputNum_log.includes(inputNum_val)) {
+      $resultText.innerText = '이미 입력하신 숫자입니다. (기회: '+chance+')';
+    } else {
+      chance --;
+
+      inputNum_log.push(inputNum_val);
+
+      if(chance == 0) $goButton.disabled = true;
+
+      if(randomNum == inputNum_val) {
+        $resultText.innerText = '맞췄습니다. (기회: '+chance+')';
+        $goButton.disabled = true;
+      } else if(randomNum < inputNum_val) {
+        $resultText.innerText = 'Down (기회: '+chance+')';
+      } else if(randomNum > inputNum_val) {
+        $resultText.innerText = 'Up (기회: '+chance+')';
+      }
+    }
+  }
+});
+
+$resetButton.addEventListener('click', function(){
+  randomNum = setRandom();
+  $inputNum.value = "";
+  $resultText.innerText = '1~50 범위 내 숫자를 입력해주세요 (기회: '+chance+')';
+  console.log('reset', chance);
+});
