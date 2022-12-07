48cf8c59dcb633ec78b45a13aaab09a6e21e6582
oncee <oncee0210@gmail.com>
2022-12-07 11:10:21 +0900

    - 숫자범위, 중복 입력 예외처리 return 추가 - $gobutton.disaled boolean값으로 처리 (gameOver) - play, reset function 처리 - $inputNum 입력값 초기화 function 처리

--- main.js
+++ main.js
@@ -15,8 +15,8 @@ const $goButton = document.querySelector('#btn-go');
 const $resetButton = document.querySelector('#btn-reset');
 const $inputNum = document.querySelector("input[name=number]");
 let inputNum_val = "";
+let gameOver = false;
 let inputNum_log = [];
-
 let randomNum = 0;
 let result = "";
 let chance = 5;
@@ -24,38 +24,54 @@ let chance = 5;
 function setRandom() {
   return Math.floor(Math.random() * 50) + 1;
 }
-randomNum = setRandom();
 
-$goButton.addEventListener('click', function(){
+function gamePlay() {
   inputNum_val = $inputNum.value;
 
   if(inputNum_val <=0 || inputNum_val > 50){
     $resultText.innerText = '1에서 50사이 숫자를 입력해주세요. (기회: '+chance+')';
-  }else{
-    if(inputNum_log.includes(inputNum_val)) {
-      $resultText.innerText = '이미 입력하신 숫자입니다. (기회: '+chance+')';
-    } else {
-      chance --;
-
-      inputNum_log.push(inputNum_val);
-
-      if(chance == 0) $goButton.disabled = true;
-
-      if(randomNum == inputNum_val) {
-        $resultText.innerText = '맞췄습니다. (기회: '+chance+')';
-        $goButton.disabled = true;
-      } else if(randomNum < inputNum_val) {
-        $resultText.innerText = 'Down (기회: '+chance+')';
-      } else if(randomNum > inputNum_val) {
-        $resultText.innerText = 'Up (기회: '+chance+')';
-      }
-    }
+    return;
   }
-});
+  
+  if(inputNum_log.includes(inputNum_val)) {
+    $resultText.innerText = '이미 입력하신 숫자입니다. (기회: '+chance+')';
+    return;
+  }
 
-$resetButton.addEventListener('click', function(){
+  chance --;
+
+  inputNum_log.push(inputNum_val);
+
+  if(chance == 0) gameOver = true;
+
+  if(randomNum == inputNum_val) {
+    $resultText.innerText = '맞췄습니다. (기회: '+chance+')';
+    gameOver = true;
+  } else if(randomNum < inputNum_val) {
+    $resultText.innerText = 'Down (기회: '+chance+')';
+  } else if(randomNum > inputNum_val) {
+    $resultText.innerText = 'Up (기회: '+chance+')';
+  }
+
+  if(gameOver == true) $goButton.disabled = true;
+}
+
+function gameReset() {
   randomNum = setRandom();
+  gameOver = false;
+  $goButton.disabled = false;
   $inputNum.value = "";
+  chance = 5;
   $resultText.innerText = '1~50 범위 내 숫자를 입력해주세요 (기회: '+chance+')';
   console.log('reset', chance);
-});
+}
+
+function inputReset() {
+  $inputNum.value = "";
+}
+
+$goButton.addEventListener('click', gamePlay);
+$resetButton.addEventListener('click', gameReset);
+$inputNum.addEventListener('click', inputReset);
+
+randomNum = setRandom();
